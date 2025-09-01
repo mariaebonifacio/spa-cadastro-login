a281429809839ac6b552ae9377c14325f23b83f5
Luiza Rangel <luiza.g.rangel@aluno.senai.br>
2025-09-01 11:32:20 -0300

    alteracoes novas

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,2 +1,15 @@
 import '.Button.css';
 
+// Componente reutilizavel que recebe 'children' (o texto do botão) 
+// e 'onClick' (a função a ser executada ao clicar).
+// O 'type' é importante para formulários ('submit' ou 'button)
+
+function Button({children, onClick, tyoe = 'button'}) {
+    return (
+        <button type={type} className='custom-button' onClick={onClick}>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,2 +1,16 @@
 import Button from '../Button/Button';
 import '/Menu.css';
+
+// Recebe a função 'onMenuClick' via props para...
+function Menu({ onMenuClick}) {
+    return (
+
+        <nav className="app-menu">
+            {/* Ao clicar, chama a função recebida, passando o nome da tela desejada */}
+            <Button onClick={() => onMenuClick('login')}>Login</Button>
+            <Button onClick={() => onMenuClick('cadastro')}>Cadastro</Button>
+        </nav>
+    );
+}
+
+export default Menu;
