b828981f75b9c253f9cd1a2d9a04b1bcaac44fa6
Luiza Rangel <luiza.g.rangel@aluno.senai.br>
2025-09-01 11:05:15 -0300

    alteracoes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import '.Header.css';
+
+function Header(){
+    return (
+
+        <header className='app-header'>
+            <h1>Compenentes React</h1>
+            <p>Uma aplicação simples para demonstrar a compenentização</p>
+        </header>
+    );
+}
+
+export default Header;
